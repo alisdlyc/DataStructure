d16d643e448ba9591d5838eac02d295e8d94039d
alisdlyc <1428717646@qq.com>
2019-10-18 22:21:34 +0800

    updata my tasks and json,learn to use VsCode

---
+++ LeetCode/TwoNumsSum.c
@@ -0,0 +1,7 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-17 19:33:43
+ * @LastEditTime: 2019-10-17 19:33:52
+ * @LastEditors: Please set LastEditors
+ */
---
+++ workspace/PxToSp.c
@@ -0,0 +1,16 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-10-18 20:11:33
+ * @LastEditTime: 2019-10-18 20:20:13
+ * @LastEditors: Please set LastEditors
+ */
+#include<stdio.h>
+int main(){
+    int px;
+    sd:scanf("%d",&px);
+    float dp=8.0*px/21.0;
+    printf("%f\n",dp);
+    goto sd;
+    return 0;
+}
