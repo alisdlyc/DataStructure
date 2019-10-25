c9b5adc388db72cf1ef04aa7feb0332684fb3c44
alisdlyc <1428717646@qq.com>
2019-10-25 21:04:43 +0800

    readme new

---
+++ workspace/FindPar.c
@@ -0,0 +1,20 @@
+/*
+ * 已知在二叉链表表示的二叉树中，root 为根结点
+ * p↑和 q↑为二叉树中两个结点，试编写算法求距离它们最近的共同祖先。
+ * 思路 定义函数，输入为节点，输出为节点的双亲数组
+ *      对两个结点的双亲数组进行遍历，最先匹配的就是他们最近的共同祖先
+ * @Author: your name
+ * @Date: 2019-10-25 18:25:17
+ * @LastEditTime: 2019-10-25 18:25:40
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \DataStructure\workspace\FindPar.c
+ */
+
+#include<stdio.h>
+
+int main(){
+
+
+    return 0;
+}
