5d6695a177fb16b0553c497ba9e8b6e1a573363a
alisdlyc <1428717646@qq.com>
2019-10-28 19:31:56 +0800

    csapp coding test

---
+++ workspace/csapp-test.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2019-10-27 16:03:12
+ * @LastEditTime: 2019-10-27 16:06:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \DataStructure\workspace\csapp-test.c
+ */
+#include<stdio.h>
+int main(){
+    if(2147483647U <(-2147483647-1))
+        printf("%d\n",-2147483647-1);
+    getchar();
+    return 0;
+}
