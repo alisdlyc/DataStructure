8c3b65d77b0ecbc410bde74756f6524abb53afea
alisdlyc <1428717646@qq.com>
2019-11-11 18:48:25 +0800

    数据结构

--- workspace/数据结构复习.c
+++ workspace/数据结构复习.c
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-11-07 17:20:14
- * @LastEditTime: 2019-11-07 18:54:56
+ * @LastEditTime: 2019-11-07 20:25:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \DataStructure\workspace\数据结构复习.c
@@ -24,3 +24,24 @@ void InitList(Sqlist* L){
     L->length=0;
     L->listsize=LIST_INIT_SIZE;
 }
+
+// 串的堆分配存储表示
+typedef struct {
+    char *ch;       // 若为非空串，则按照串长分配存储空间，否则ch为NULL
+    int length;     // 串长
+}HString;
+
+void Init(HString &T,char* chars){
+    T.ch=(char*)malloc(sizeof(char)*strlen(chars));
+    T.lenght=strlen(chars);
+}
+
+typedef struct {
+    char str[ MAXLEN];
+    int  curlen;
+} string;
+
+typedef struct node {
+    char  ch [MaxLen];  //块链大小
+    struct  node  *next;
+}node,  * pointer;   
