786013f062416944a132aea0338e62a9eca2a523
alisdlyc <1428717646@qq.com>
2019-11-24 18:45:25 +0800

    nothing

--- workspace/Csapp.c
+++ workspace/Csapp.c
@@ -1,15 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2019-11-09 14:43:06
- * @LastEditTime: 2019-11-11 18:32:47
+ * @LastEditTime: 2019-11-23 21:54:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \DataStructure\workspace\Csapp.c
  */
 #include<stdio.h>
 int main(){
-    printf("Max of int 32 %d\n",__INT32_MAX__);
-    printf("Max + 1 is    %d\n",__INT32_MAX__+1);
-    printf("%d",-(__INT32_MAX__+1));
+    printf("%s",(char*)0x45374fee);
     getchar();
 }
