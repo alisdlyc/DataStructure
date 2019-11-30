2b436057fb33366b1ba494ac59aff3187cc8e0d2
alisdlyc <1428717646@qq.com>
2019-11-30 10:54:49 +0800

    Anki服务器配置文档

---
+++ workspace/mysyncserver.py
@@ -0,0 +1,11 @@
+'''
+@Author: your name
+@Date: 2019-11-28 17:17:43
+@LastEditTime: 2019-11-28 17:18:59
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \DataStructure\workspace\mysyncserver.py
+'''
+import anki.sync
+anki.sync.SYNC_BASE = 'http://182.92.193.59:27701/'
+anki.sync.SYNC_MEDIA_BASE = 'http://182.92.193.59:27701/msync/'
