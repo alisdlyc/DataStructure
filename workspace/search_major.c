8ba3817e0d430ecbd22f8726712d9708bf575f40
alisdlyc <1428717646@qq.com>
2019-12-01 19:19:08 +0800

    频度搜索

--- workspace/search_major.c
+++ workspace/search_major.c
@@ -1,47 +1,37 @@
+/*
+ * @Author: your name
+ * @Date: 2019-11-30 14:05:38
+ * @LastEditTime: 2019-12-01 19:18:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \DataStructure\workspace\search_major.c
+ */
 /* 查找序列以带头结点的单链表表示，各结点中设一个访问频度，
  *      初始值为 0，每次查找成功后该结点频度值增加 1。试给出算法，在每次查找后查找序列均按访问频度从大到小排列。
  */
 
-#include <stdio.h>
-#include<malloc.h>
 typedef struct Node
 {
     int data;
     int freq;
-    Node* next;
+    struct Node *next;
 }Node;
 
-void ListCreat(Node* HeadPtr){
-    int a=10;
-    Node* CurrentPtr;
-    int head=0;
-
-    while(a){
-        Node* NextPtr=(Node*)malloc(sizeof(Node));
-        NextPtr->data=a;
-        NextPtr->freq=0;
-        if(!head){
-            HeadPtr=NextPtr;
-            CurrentPtr=HeadPtr;
-            head=1;
-        }else
-        {
-            CurrentPtr->next=NextPtr;
-            CurrentPtr=NextPtr;
-        }
-        CurrentPtr->next=NULL;
-        a--;
-    }
-}
 int ListSearch(Node* HeadPtr,int key){
     Node* Ptr=HeadPtr;
-    int prim=0;
-    while(Ptr!=NULL){
-        
+    int re;
+    while(Ptr->next){
+        if(Ptr->data==key){
+            re=key;
+            Ptr->freq++;
+            break;
+        }
     }
-    return prim;
+    ListSort(HeadPtr);
+    return re;
 }
 
+// 搜索完毕后，重新排列搜索频度
 void ListSort(Node *HeadReList) {
 	int n, m;
 	Node* Ptr;
@@ -49,7 +39,7 @@ void ListSort(Node *HeadReList) {
 
 	for (int i = 0; i < len; i++) {
 		Ptr = HeadReList;
-		for (int j = 0; j < len - 1; j++) {
+		for (int j = i; j < len - 1; j++) {
 			if (Ptr->freq < Ptr->next->freq) {
 				int data_m = Ptr->data;
                 int data_freq=Ptr->freq;
@@ -61,42 +51,4 @@ void ListSort(Node *HeadReList) {
 			Ptr = Ptr->next;
 		}
 	}
-}
-
-void OutPut(Node* L) {
-	Node *p = L;
-	printf("查询表为:");
-
-	while (p!=NULL){
-		printf("%d", p->data);
-		if (p->next != NULL)
-			printf(" ");
-		else
-			printf("\n");
-		p = p->next;
-	}
-    p = L;
-    printf("freq:");
-	while (p!=NULL){
-		printf("%d", p->freq);
-		if (p->next != NULL)
-			printf(" ");
-		else
-			printf("\n");
-		p = p->next;
-	}
-}
-
-int main(){
-    Node *HeadPtr=(Node*)malloc(sizeof(Node));
-    ListCreat(HeadPtr);
-    start:printf("请输入要查询的内容\n");
-    int key;
-    scanf("%d",&key);
-    int re=ListSearch(HeadPtr,key);
-    printf("%d\n",re);
-    // ListSort(HeadPtr);
-    OutPut(HeadPtr);
-    goto start;
-    return 0;
 }
