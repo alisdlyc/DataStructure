5329ad456bc31c57810eb087e5bcf4bcfeb31578
alisdlyc <1428717646@qq.com>
2019-12-07 21:33:47 +0800

    px to dp

--- workspace/PxToSp.c
+++ workspace/PxToSp.c
@@ -2,14 +2,14 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-10-18 20:11:33
- * @LastEditTime: 2019-10-18 20:20:13
+ * @LastEditTime: 2019-12-07 16:11:20
  * @LastEditors: Please set LastEditors
  */
 #include<stdio.h>
 int main(){
     int px;
     sd:scanf("%d",&px);
-    float dp=8.0*px/21.0;
+    float dp=160*px/420.0;
     printf("%f\n",dp);
     goto sd;
     return 0;
