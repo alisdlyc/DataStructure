c645c18d50edcac733f34dcdc64bff633f8026e5
alisdlyc <1428717646@qq.com>
2020-01-01 07:53:14 +0800

    Csapp Debug

--- workspace/Csapp.c
+++ workspace/Csapp.c
@@ -1,13 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2019-11-09 14:43:06
- * @LastEditTime: 2019-11-23 21:54:42
- * @LastEditors: Please set LastEditors
+ * @LastEditTime : 2020-01-01 07:50:27
+ * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \DataStructure\workspace\Csapp.c
  */
 #include<stdio.h>
 int main(){
-    printf("%s",(char*)0x45374fee);
+    for(int x=0;x<10;x++){
+        for(int y=0;y<=10;y++){
+            printf("  %d\tX:%d\tY:%d\n",(!x|!!y)==1,x,y);
+        }
+    }
+
     getchar();
 }
