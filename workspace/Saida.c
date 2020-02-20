4e328befc947b03598f235a8138f91c8a6603534
alisdlyc <1428717646@qq.com>
2020-02-20 22:59:46 +0800

    text

---
+++ workspace/Saida.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-01 15:38:24
+ * @LastEditTime : 2020-01-01 15:42:16
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \DataStructure\workspace\Saida.c
+ */
+#include<stdio.h>
+int main(){
+    double world;
+    unsigned letter;
+    short stay;
+    long memories;
+    printf("i miss you.\n");
+}
+
+/*
+ *  两个世界，一封失信，短暂相陪，长久回忆。
+ *  唯留心头一句： “想你！"
+ *  倏然打印。
+ *  进程却已抹去。
+ */
